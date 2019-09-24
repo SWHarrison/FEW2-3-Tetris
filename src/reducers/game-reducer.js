d233c670adcb01caa0cc22b8d17bcc693ff2bad1
Sam Harrison <samuel.harrison@students.makeschool.com>
2019-09-24 13:27:03 -0700

    added reducer for moving down

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,4 +1,10 @@
-import { defaultState, nextRotation, canMoveTo } from '../utils'
+import {  defaultState,
+          nextRotation,
+          canMoveTo,
+          addBlockToGrid,
+          checkRows,
+          randomShape
+        } from '../utils'
 
 import {
   MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
@@ -30,8 +36,31 @@ const gameReducer = (state = defaultState(), action) => {
       return state
 
     case MOVE_DOWN:
-
-      return state
+      // Get the next potential Y position
+      const maybeY = y + 1
+      // Check if the current block can move here
+      if (canMoveTo(shape, grid, x, maybeY, rotation)) {
+        // If so move the block
+        return { ...state, y: maybeY }
+      }
+      // If not place the block
+      const newGrid = addBlockToGrid(shape, grid, x, y, rotation)
+      // reset some things to start a new shape/block
+      const newState = defaultState()
+      newState.grid = newGrid
+      newState.shape = nextShape
+      newState.nextShape = randomShape()
+      newState.score = score
+      newState.isRunning = isRunning
+      if (!canMoveTo(nextShape, newGrid, 0, 4, 0)) {
+        // Game Over
+        console.log("Game Should be over...")
+        newState.shape = 0
+        return { ...state, gameOver: true }
+      }
+      // Update the score based on if rows were completed or not
+      newState.score = score + checkRows(newGrid)
+      return newState
 
     case RESUME:
 
